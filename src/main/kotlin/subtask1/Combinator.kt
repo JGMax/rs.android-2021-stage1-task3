fb31b9b9f71f7a6f42e56ab05683bb1c4e5244ba
JGMax <maksus20.05@mail.ru>
2021-07-04 22:36:37 +0300

    feat: subtask1 solution

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,27 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
+    private fun Int.factorial(stop: Int = 2) : Long {
+        if (this <= 1) {
+            return 1
+        }
+        var result = 1L
+        for (i in this downTo stop) {
+            result *= i
+        }
+        return result
+    }
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        val nFac = array[1].factorial()
+
+        for (x in 1..n) {
+            if (m <= nFac/(x.factorial() * (n - x).factorial())) {
+                return x
+            }
+        }
+        return null
     }
 }
