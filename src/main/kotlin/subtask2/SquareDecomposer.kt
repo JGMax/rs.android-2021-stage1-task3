9fd4b45f67b2537a377c57dcf9ba8807d8fa2081
JGMax <maksus20.05@mail.ru>
2021-07-05 13:42:08 +0300

    feat: add subtask3 solution

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,33 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val targetSum = number * number
+        return findDecomposition(targetSum, number - 1)?.toTypedArray()
+    }
+
+    private fun findDecomposition(target: Int, limit: Int): ArrayList<Int>? {
+        for (i in limit downTo 1) {
+            val square = i * i
+            if (target - square == 0) {
+                return arrayListOf(i)
+            }
+            if (target - square < 0) {
+                return null
+            }
+            var newLimit = sqrt((target - square).toDouble()).toInt()
+            if (newLimit >= i) {
+                newLimit = i - 1
+            }
+            val result = findDecomposition(target - square, newLimit)
+            if (result != null) {
+                result.add(i)
+                return result
+            }
+        }
+        return null
     }
 }
